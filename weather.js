805506018b391622ffd2305f706f14afabb9855a
aditya prakash pandey <pandey2app@gmail.com>
2024-05-02 15:28:53 +0000

    added page loader functionallity

--- weather.js
+++ weather.js
@@ -101,3 +101,28 @@ class City {
     }
 
 }
+
+function pageLoader(page) {
+    if (page === 'home') {
+        desiredPage = 'home';
+        renderHome();
+        isAllCitiesActive = false;
+        isSearchActive = false;
+        isHomeActive = true;
+        activePage = 'home';
+    } else if (page === 'allCities') {
+        desiredPage = 'allCities';
+        renderAllCities();
+        isHomeActive = false;
+        isSearchActive = false;
+        isAllCitiesActive = true;
+        activePage = 'allCities';
+    } else if (page === 'searchCity') {
+        desiredPage = 'searchCity';
+        renderSearch();
+        isHomeActive = false;
+        isAllCitiesActive = false;
+        isSearchActive = true;
+        activePage = 'searchCity';
+    }
+}
